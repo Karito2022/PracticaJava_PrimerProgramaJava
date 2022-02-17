9153bd17429bebd63b490a808015f31ae36a8107
Karito2022 <reyesbarrerakaren@gmail.com>
2022-02-17 11:35:12 -0500

    PracticaJava_PrimerProgramaJava

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Mi nombre es Denisse");
+		System.out.println("Tengo 30 años de edad");
+		System.out.println("Mi ciudad natal es Huancayo");
+	}
+}
